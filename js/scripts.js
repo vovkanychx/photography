20962e0b3c9830937c7f929a0082d589ba257239
Vovkanych Dmytro <dima.vovkanych24@gmail.com>
2021-12-16 00:02:24 +0200

    commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.main_slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        adaptiveHeight: true
+      });
+  });
