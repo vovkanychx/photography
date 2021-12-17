a282ca32913f9e3e962a8f5b6f650d492b109ff9
Vovkanych Dmytro <dima.vovkanych24@gmail.com>
2021-12-17 02:24:36 +0200

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,45 @@
+//Get the button
+var topButton = document.getElementById("topButton");
+
+// When the user scrolls down 500px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+// Active menu elements
+var btns = document.getElementsByClassName("header_menu_li");
+
+// Go through every menu element to add active class if clicked
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+// Slick-slider main page options
 $(document).ready(function(){
     $('.main_slider').slick({
-        dots: true,
         infinite: true,
-        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        speed: 1000,
         slidesToShow: 1,
-        adaptiveHeight: true
+        fade: true,
+        pauseOnFocus: true,
+        pauseOnHover: false
       });
   });
